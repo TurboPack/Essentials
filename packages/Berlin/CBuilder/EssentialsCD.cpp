5127dde79e34e0d7002bcd8ae9173db01f58ba02
Roman Kassebaum <roman@kassebaum.eu>
2019-09-06 01:49:23 +0200

    Added support for Berlin.

---
+++ packages/Berlin/CBuilder/EssentialsCD.cpp
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#include <System.hpp>
+#pragma hdrstop
+USEFORMNS("..\..\..\source\EsAbout0.pas", Esabout0, EsAboutForm);
+USEFORMNS("..\..\..\source\EsLabel1.pas", Eslabel1, SaveSchemeFrm);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package-Quelltext.
+//---------------------------------------------------------------------------
+
+
+#pragma argsused
+extern "C" int _libmain(unsigned long reason)
+{
+	return 1;
+}
+//---------------------------------------------------------------------------
